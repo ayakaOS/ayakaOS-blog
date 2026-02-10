f3ebe7c64ecd4ffb6b22a0d2d5a236340cb0f8d1
Florian-Su <92657591+Florian-Su@users.noreply.github.com>
2026-02-10 14:45:05 +0800

    Update hi-card.tsx

--- src/app/(home)/hi-card.tsx
+++ src/app/(home)/hi-card.tsx
@@ -48,7 +48,7 @@ export default function HiCard() {
 				)}
 				<img src='/images/avatar.png' className='mx-auto rounded-full' style={{ width: 120, height: 120, boxShadow: ' 0 16px 32px -5px #E2D9CE' }} />
 				<h1 className='font-averia mt-3 text-2xl'>
-					{greeting} <br /> We're <span className='text-linear text-[32px]'>{username}</span> , Nice 2 <br /> meet u!
+					{greeting} <br /> 我们是 <span className='text-linear text-[32px]'>{username}</span> , 很高兴 <br /> 认识你!
 				</h1>
 			</Card>
 		</HomeDraggableLayer>
